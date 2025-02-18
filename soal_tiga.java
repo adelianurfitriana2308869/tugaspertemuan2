af098e1957bb28e11c78cc9a683a53a22e32947c
Adelia Nur Fitriana <adeliaanf@gmail.com>
2025-02-18 19:02:37 +0700

    Update soal_tiga.java

--- soal_tiga.java
+++ soal_tiga.java
@@ -4,31 +4,22 @@ karakter ‘2’!
  */
 package tugas2;
 
-import java.util.Scanner;
-
-
 
 public class soal_tiga {
      public static void main(String[] args){
         
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Masukkan karakter pertama: ");
-        char char1 = scanner.next().charAt(0);
-        
-        System.out.print("Masukkan karakter kedua: ");
-        char char2 = scanner.next().charAt(0);
+        char char1 = '1';
+        char char2 = '2';
+
        
         int ascii1 = (int) char1;
         int ascii2 = (int) char2;
         int jumlahAscii = ascii1 + ascii2;
- 
-        System.out.println("\nKode ASCII dari karakter '" + char1 + "': " + ascii1);
-        System.out.println("Kode ASCII dari karakter '" + char2 + "': " + ascii2);
-        System.out.println("Jumlah kode ASCII dari kedua karakter: " + jumlahAscii);
+
         
-        scanner.close();
-     
+        System.out.println("Kode ASCII dari karakter '" + char1 + "': " + ascii1);
+        System.out.println("Kode ASCII dari karakter '" + char2 + "': " + ascii2);
+        System.out.println("Jumlah kode ASCII dari '" + char1 + "' dan '" + char2 + "': " + jumlahAscii);
      
      
      }
